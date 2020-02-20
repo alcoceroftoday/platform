141b8fa2da7811bdd4c97f3d218b5517aa334f96
David Alcocer <davidbryan.alcocer@processconsulting.net>
2020-02-20 11:03:46 -0500

    Solucion error -fail import react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 
-const App=()=> (
-    <div >
+function App() {
+  return (
+    <div className="App">
       Hi
     </div>
-)
+  );
+}
 
 export default App;
